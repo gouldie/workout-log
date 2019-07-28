ae8964a94c73b6b4f77273fa4dc2067243b29113
Matt <mgould@breakingfreegroup.com>
2019-07-28 08:25:42 +0100

    accounts

--- src/routes/accounts/index.js
+++ src/routes/accounts/index.js
@@ -1,6 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { Login, Register } from '../../components/accounts'
 
-export default () => (
-  <div>accounts route</div>
+const Screen = (props) => {
+  switch (props.match.path) {
+    case '/login':
+      return <Login {...props} />
+    case '/register':
+      return <Register {...props} />
+    default:
+      // Redirect!
+      return <div></div>
+  }
+}
+
+export default (props) => (
+  <div id="container">
+    <Screen {...props} />
+  </div>
 )
