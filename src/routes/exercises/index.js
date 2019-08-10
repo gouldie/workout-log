86decbd7120f164f85fd696bd4db278ac922b1bd
Matt <mgould@breakingfreegroup.com>
2019-08-10 11:26:53 +0100

    exercises list filter

--- src/routes/exercises/index.js
+++ src/routes/exercises/index.js
@@ -4,22 +4,57 @@ import exercises from '../../assets/json/exercises.json'
 import Exercise from '../../components/exercises/exercise'
 // import { Container } from '../../components/core'
 import Container from '@material-ui/core/Container'
+import IconButton from '@material-ui/core/IconButton'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import CloseIcon from '@material-ui/icons/Close'
 
 class Exercises extends Component {
   constructor () {
     super()
 
     this.state = {
-      hover: null
+      filter: ''
     }
   }
 
+  filterOnChange = (e) => {
+    this.setState({ filter: e.target.value })
+  }
+
+  clearFilter = () => {
+    this.setState({ filter: '' })
+  }
+
   render () {
+    const { filter } = this.state
+
+    const filteredExercises = exercises && exercises.filter(e => e.name.toLowerCase().includes(filter.toLowerCase()))
+
     return (
       <Container maxWidth='sm'>
+        <TextField
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <CloseIcon style={{ color: 'gray', cursor: 'pointer' }} onClick={this.clearFilter} />
+              </InputAdornment>
+            )
+          }}
+          value={filter}
+          onChange={this.filterOnChange}
+          placeholder='Filter'
+          style={{
+            maxWidth: '500px',
+            width: '100%',
+            borderRadius: '5px',
+            backgroundColor: '#fff',
+            padding: '5px 10px',
+            marginBottom: '20px'
+          }}
+        />
         <div className='exercise-container flex column' style={{ width: '100%' }}>
           {
-            exercises && exercises.map((e, i) => (
+            filteredExercises && filteredExercises.map((e, i) => (
               <Exercise
                 key={i}
                 data={{ ...e }}
