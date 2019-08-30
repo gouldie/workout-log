024137ea6c441efb0bc4b8c78257d913a69fe70a
Matt <mgould@breakingfreegroup.com>
2019-08-30 08:03:12 +0100

    wip

--- src/components/routines/routine.js
+++ src/components/routines/routine.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
+import { Link } from 'react-router-dom'
 
 export default ({ data }) => {
   return (
-    <Paper className='exercise flex justify-between align-items-start'>
-      <div className='flex column justify-between' style={{ height: '100%' }}>
-        <div>
-          <strong>Name</strong>
-          <p style={{ marginBottom: '10px' }}>{data.name}</p>
+    <Link to={`/routine/${data._id}`}>
+      <Paper className='exercise flex justify-between align-items-start'>
+        <div className='flex column justify-between' style={{ height: '100%' }}>
+          <div>
+            <strong>Name</strong>
+            <p style={{ marginBottom: '10px' }}>{data.name}</p>
+          </div>
+          <div>
+            <strong>Description</strong>
+            <p>{data.desc}</p>
+          </div>
         </div>
-        <div>
-          <strong>Description</strong>
-          <p>{data.desc}</p>
-        </div>
-      </div>
-      <p style={{ marginBottom: '10px' }}>{`${Object.keys(data.days).length} days/week`}</p>
-    </Paper>
+        <p style={{ marginBottom: '10px' }}>{`${Object.keys(data.days).length} days/week`}</p>
+      </Paper>
+    </Link>
   )
 }
